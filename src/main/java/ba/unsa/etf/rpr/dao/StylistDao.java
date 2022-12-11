dc0e5045cff7aca9357807b3d4b2e4dfa013f23b
Nejra <nadilovic2@etf.unsa.ba>
2022-12-11 15:23:46 +0100

    Added StylistDao interface

---
+++ src/main/java/ba/unsa/etf/rpr/dao/StylistDao.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Stylist;
+
+/**
+ * Dao interface for Stylist domain bean
+ *
+ * @author Nejra Adilović
+ */
+public interface StylistDao extends Dao<Stylist> {
+
+}
