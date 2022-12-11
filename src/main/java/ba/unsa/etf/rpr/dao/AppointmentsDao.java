36870a7890a8c3b888f4cb8c08e84e5310e57a23
Nejra <nadilovic2@etf.unsa.ba>
2022-12-11 15:25:29 +0100

    Added AppointmentsDao interface

---
+++ src/main/java/ba/unsa/etf/rpr/dao/AppointmentsDao.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Appointments;
+
+/**
+ * Dao interface for Appointments domain bean
+ *
+ * @author Nejra Adilović
+ */
+public interface AppointmentsDao extends Dao<Appointments> {
+
+}
