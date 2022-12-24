0b6c8c66ba45ae85ff5d1c8c40dd711101c80ede
Nejra <nadilovic2@etf.unsa.ba>
2022-12-24 13:14:53 +0100

    Refactoring of Dao class

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -1,7 +1,7 @@
 package ba.unsa.etf.rpr.dao;
 
 import java.util.List;
-
+import ba.unsa.etf.rpr.exceptions.HairsalonException;
 /**
  * Root interface for all DAO classes
  *
@@ -14,31 +14,31 @@ public interface Dao<T> {
      * @param id primary key of entity
      * @return Entity from database
      */
-    T getById(int id);
+    T getById(int id) throws HairsalonException;
 
     /**
      * Saves entity into database
      * @param item bean for saving to database
      * @return saved item with id field populated
      */
-    T add(T item);
+    T add(T item) throws HairsalonException;
 
     /**
      * Fully updates entity in database based on id (primary) match.
      * @param item - bean to be updated. id must be populated
      * @return updated version of bean
      */
-    T update(T item);
+    T update(T item) throws HairsalonException;
 
     /**
      * Hard delete of item from database with given id
      * @param id - primary key of entity
      */
-    void delete(int id);
+    void delete(int id) throws HairsalonException;
 
     /**
      * Lists all entities from database. WARNING: Very slow operation because it reads all records.
      * @return List of entities from database
      */
-    List<T> getAll();
+    List<T> getAll() throws HairsalonException;
 }
