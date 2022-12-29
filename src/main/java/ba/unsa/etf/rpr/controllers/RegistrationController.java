cc48caa37e416f7fd28bf502db88d687493ea8b2
Nejra <nadilovic2@etf.unsa.ba>
2022-12-29 14:19:01 +0100

    Added initialize function in RegistrationController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.application.Platform;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 
@@ -14,4 +15,15 @@ public class RegistrationController{
     public Hyperlink loginLink;
     public Label invalidUsername;
     public GridPane gridPaneRegistration;
+
+    public void initialize(){
+        Platform.runLater(()->firstnameEntry.setFocusTraversable(false));
+        String username=usernameEntry.getText();
+        usernameEntry.textProperty().addListener((obs, oldValue, newValue)->{
+            if(newValue.length()>=6)
+                invalidUsername.setText("");
+            else if(newValue.length()<6)
+                invalidUsername.setText("Username must have atleast 6 characters");
+        });
+    }
 }
