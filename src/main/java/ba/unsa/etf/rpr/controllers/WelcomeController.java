eb23f79436097b808d3591bba226a32cd37ccafc
Nejra <nadilovic2@etf.unsa.ba>
2022-12-29 14:32:47 +0100

    Added mainWindowOnMouseClicked function in WelcomeController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/WelcomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/WelcomeController.java
@@ -1,9 +1,27 @@
 package ba.unsa.etf.rpr.controllers;
 
-
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 public class WelcomeController {
     public ImageView appointmentId;
     public AnchorPane anchorPaneId;
+    public void mainWindowOnMouseClicked(MouseEvent mouseEvent) throws IOException {
+        final Stage mainStage = (Stage) anchorPaneId.getScene().getWindow();
+        Stage myStage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/mainWindow.fxml"));
+        loader.load();
+        myStage.setTitle("Seat&Style");
+        myStage.getIcons().add(new Image("/img/loginlogo.png"));
+        myStage.setScene(new Scene(loader.getRoot(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        myStage.show();
+        mainStage.hide();
+    }
 }
