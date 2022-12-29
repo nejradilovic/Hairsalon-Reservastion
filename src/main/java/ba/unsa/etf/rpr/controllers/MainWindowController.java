3a25c00ef6669ab5451b6568eb96a992885e5862
Nejra <nadilovic2@etf.unsa.ba>
2022-12-29 14:39:28 +0100

    Added attributes in MainWindowController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/MainWindowController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainWindowController.java
@@ -1,5 +1,15 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 public class MainWindowController {
-
+    public Button logoutId;
+    public BorderPane borderPaneId;
+    public ImageView logoutIcon;
+    public HBox stylingHBoxId;
+    public Label stylingTextId;
+    public ImageView stylingImageId;
 }
