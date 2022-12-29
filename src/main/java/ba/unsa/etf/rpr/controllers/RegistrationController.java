3da75c7dbd524038c23f34dfac6b8bd8dd1577b4
Nejra <nadilovic2@etf.unsa.ba>
2022-12-29 14:25:53 +0100

    Added signupOnAction function in RegistrationController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -9,6 +9,7 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import java.io.IOException;
+
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class RegistrationController{
@@ -43,5 +44,29 @@ public class RegistrationController{
         myStage.setScene(new Scene(loader.getRoot(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         myStage.show();
         registrationStage.hide();
+    }
+
+    public void signupOnAction(ActionEvent actionEvent) throws IOException {
+        if(usernameEntry.getText().length()<6) signupButton.setDisable(true);
+        if(usernameEntry.getText().isBlank()==true || lastnameEntry.getText().isBlank()==true || firstnameEntry.getText().isBlank()==true || emailEntry.getText().isBlank()==true
+                || phoneEntry.getText().isBlank()==true || passwordEntry.getText().isBlank()==true){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning Dialog");
+            alert.setHeaderText("Missing fields");
+            alert.setContentText("You have to fill in all fields in order to register!");
+            alert.showAndWait();
+        }
+        else if(usernameEntry.getText().isBlank()==false && lastnameEntry.getText().isBlank()==false && firstnameEntry.getText().isBlank()==false && emailEntry.getText().isBlank()==false
+                && phoneEntry.getText().isBlank()==false && passwordEntry.getText().isBlank()==false){
+            final Stage mainStage = (Stage) gridPaneRegistration.getScene().getWindow();
+            Stage myStage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/welcome.fxml"));
+            loader.load();
+            myStage.setTitle("Seat&Style");
+            myStage.getIcons().add(new Image("/img/loginlogo.png"));
+            myStage.setScene(new Scene(loader.getRoot(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            myStage.show();
+            mainStage.hide();
+        }
     }
 }
