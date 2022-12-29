1f14ed62bcb3d2bb87288925444603a2dfdcbd1f
Nejra <nadilovic2@etf.unsa.ba>
2022-12-29 14:17:01 +0100

    Added attributes in RegistrationController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -1,5 +1,17 @@
 package ba.unsa.etf.rpr.controllers;
 
-public class RegistrationController{
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 
+public class RegistrationController{
+    public TextField firstnameEntry;
+    public TextField lastnameEntry;
+    public TextField emailEntry;
+    public TextField phoneEntry;
+    public TextField usernameEntry;
+    public PasswordField passwordEntry;
+    public Button signupButton;
+    public Hyperlink loginLink;
+    public Label invalidUsername;
+    public GridPane gridPaneRegistration;
 }
