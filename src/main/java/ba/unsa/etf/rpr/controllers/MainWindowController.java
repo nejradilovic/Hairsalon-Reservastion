30e66dd5a52a25d3b0d201a1588b795c69ceffb9
Nejra <nadilovic2@etf.unsa.ba>
2022-12-29 14:43:54 +0100

    Added stylingHBoxIdOnMouseClicked function in MainWindowController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/MainWindowController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainWindowController.java
@@ -48,4 +48,15 @@ public class MainWindowController {
         myStage.show();
         registrationStage.hide();
     }
+    public void stylingHBoxIdOnMouseClicked(MouseEvent mouseEvent) throws IOException {
+        final Stage registrationStage=(Stage) borderPaneId.getScene().getWindow();
+        Stage myStage=new Stage();
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/styling.fxml"));
+        loader.load();
+        myStage.setTitle("LogIn");
+        myStage.getIcons().add(new Image("/img/loginlogo.png"));
+        myStage.setScene(new Scene(loader.getRoot(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        myStage.show();
+        registrationStage.hide();
+    }
 }
