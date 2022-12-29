bfcb47b3ceb67c10e2e35019d54260a81e7282a6
Nejra <nadilovic2@etf.unsa.ba>
2022-12-29 14:10:52 +0100

    Added registerUser function in LoginController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -1,12 +1,17 @@
 package ba.unsa.etf.rpr.controllers;
-
-
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.text.Text;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class LoginController {
     public ColumnConstraints desnidio;
@@ -21,5 +26,16 @@ public class LoginController {
     public void cancelButtonOnAction(ActionEvent event){
         Stage stage = (Stage) cancelButton.getScene().getWindow();
         stage.close();
+    }
+    public void registerUser(ActionEvent actionEvent) throws IOException {
+        final Stage loginStage=(Stage) gridPane.getScene().getWindow();
+        Stage myStage=new Stage();
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/registration.fxml"));
+        loader.load();
+        myStage.setTitle("Registration");
+        myStage.getIcons().add(new Image("/img/loginlogo.png"));
+        myStage.setScene(new Scene(loader.getRoot(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        myStage.show();
+        loginStage.hide();
     }
 }
