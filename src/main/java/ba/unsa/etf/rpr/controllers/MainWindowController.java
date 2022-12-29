1132ee56a892f6d7f7ad0a3323dfc9060324c858
Nejra <nadilovic2@etf.unsa.ba>
2022-12-29 14:43:10 +0100

    Added logoutOnMouseClicked function in MainWindowController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/MainWindowController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainWindowController.java
@@ -7,11 +7,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 public class MainWindowController {
@@ -22,6 +25,18 @@ public class MainWindowController {
     public Label stylingTextId;
     public ImageView stylingImageId;
     public void logoutOnAction(ActionEvent actionEvent) throws IOException {
+        final Stage registrationStage=(Stage) borderPaneId.getScene().getWindow();
+        Stage myStage=new Stage();
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        loader.load();
+        myStage.setTitle("LogIn");
+        myStage.getIcons().add(new Image("/img/loginlogo.png"));
+        myStage.setScene(new Scene(loader.getRoot(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        myStage.setResizable(false);
+        myStage.show();
+        registrationStage.hide();
+    }
+    public void logoutOnMouseClicked(MouseEvent mouseEvent) throws IOException {
         final Stage registrationStage=(Stage) borderPaneId.getScene().getWindow();
         Stage myStage=new Stage();
         FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
