2b1fdae21cdd395e8da3c68139064a9605d88e60
Nejra <nadilovic2@etf.unsa.ba>
2022-12-29 14:06:36 +0100

    Added attributes in LoginController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -1,5 +1,18 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.scene.control.*;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.text.Text;
+import javafx.scene.layout.GridPane;
+
 public class LoginController {
+    public ColumnConstraints desnidio;
+    public Hyperlink registration;
+    public Button cancelButton;
+    public  Button loginButton;
+    public Text emptyInput;
+    public  TextField usernameTextField;
+    public  PasswordField passwordTextField;
+    public  GridPane gridPane;
 
 }
