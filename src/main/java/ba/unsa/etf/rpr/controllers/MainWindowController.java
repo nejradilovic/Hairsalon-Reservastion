2a54cfcb32e3527c108d862c8a1eb5f35651c67e
Nejra <nadilovic2@etf.unsa.ba>
2022-12-29 14:45:02 +0100

    Added treatmentHBoxIdOnMouseClicked function in MainWindowController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/MainWindowController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainWindowController.java
@@ -70,4 +70,15 @@ public class MainWindowController {
         myStage.show();
         registrationStage.hide();
     }
+    public void treatmentHBoxIdOnMouseClicked(MouseEvent mouseEvent) throws IOException {
+        final Stage registrationStage=(Stage) borderPaneId.getScene().getWindow();
+        Stage myStage=new Stage();
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/treatment.fxml"));
+        loader.load();
+        myStage.setTitle("LogIn");
+        myStage.getIcons().add(new Image("/img/loginlogo.png"));
+        myStage.setScene(new Scene(loader.getRoot(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        myStage.show();
+        registrationStage.hide();
+    }
 }
