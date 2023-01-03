bb1915e4e0a971c901172e463ae8936d09729683
Nejra <nadilovic2@etf.unsa.ba>
2023-01-03 12:53:03 +0100

    Added function openWindow in OpenNewStage.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/OpenNewStage.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/OpenNewStage.java
@@ -1,5 +1,24 @@
 package ba.unsa.etf.rpr.controllers;
 
-public class OpenNewStage {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import java.io.IOException;
 
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class OpenNewStage {
+    public<Tip extends Pane> void openWindow(Tip pane, String name) throws IOException {
+        final Stage mainStage = (Stage) pane.getScene().getWindow();
+        Stage myStage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/"+name+".fxml"));
+        loader.load();
+        myStage.setTitle("Seat&Style");
+        myStage.getIcons().add(new Image("/img/loginlogo.png"));
+        myStage.setScene(new Scene(loader.getRoot(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        myStage.show();
+        mainStage.hide();
+    }
 }
