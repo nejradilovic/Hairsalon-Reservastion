995e3e1db37932092c922fe286dff8b9eae13006
Nejra <nadilovic2@etf.unsa.ba>
2023-01-03 13:18:00 +0100

    Clean up of code in loginOnAction function

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -41,15 +41,7 @@ public class LoginController {
             emptyInput.setText("Please enter your password.");
         }
         else if(usernameTextField.getText().isBlank()==false && passwordTextField.getText().isBlank()==false) {
-            final Stage mainStage = (Stage) gridPane.getScene().getWindow();
-            Stage myStage = new Stage();
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/welcome.fxml"));
-            loader.load();
-            myStage.setTitle("Seat&Style");
-            myStage.getIcons().add(new Image("/img/loginlogo.png"));
-            myStage.setScene(new Scene(loader.getRoot(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-            myStage.show();
-            mainStage.hide();
+            o.openWindow(gridPane,"welcome");
         }
     }
 }
