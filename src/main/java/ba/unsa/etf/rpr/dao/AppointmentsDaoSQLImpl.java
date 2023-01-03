d9191a26b5dae8ac2ed6fb0e2bc683aaddc8bcac
Nejra <nadilovic2@etf.unsa.ba>
2023-01-03 18:00:10 +0100

    Small changes in AppointmentsDaoSQLImpl class

--- src/main/java/ba/unsa/etf/rpr/dao/AppointmentsDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AppointmentsDaoSQLImpl.java
@@ -1,7 +1,6 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Appointments;
-import ba.unsa.etf.rpr.domain.Stylist;
 import ba.unsa.etf.rpr.exceptions.HairsalonException;
 
 import java.sql.*;
