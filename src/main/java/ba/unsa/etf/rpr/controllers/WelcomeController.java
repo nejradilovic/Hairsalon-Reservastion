53ebbd877ec6e0b609a9ab62fe376316068d7bce
Nejra <nadilovic2@etf.unsa.ba>
2023-01-03 12:56:14 +0100

    Clean up of code in WelcomeController class

--- src/main/java/ba/unsa/etf/rpr/controllers/WelcomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/WelcomeController.java
@@ -1,16 +1,9 @@
 package ba.unsa.etf.rpr.controllers;
-
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import ba.unsa.etf.rpr.controllers.OpenNewStage;
 
 import java.io.IOException;
-import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 public class WelcomeController {
     public ImageView appointmentId;
     public AnchorPane anchorPaneId;
