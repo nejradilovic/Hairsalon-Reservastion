e9331caaed4ff0290361d8fa995e1aa80b3e91c6
Nejra <nadilovic2@etf.unsa.ba>
2023-01-03 13:15:19 +0100

    Clean up of code in MainWindowController class

--- src/main/java/ba/unsa/etf/rpr/controllers/MainWindowController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainWindowController.java
@@ -1,22 +1,14 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
 
 import java.io.IOException;
-
-import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
-
-import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 public class MainWindowController {
     public Button logoutId;
     public BorderPane borderPaneId;
