dbb9634bb0708cdf36a5079dd7f0c139b76fa643
Nejra <nadilovic2@etf.unsa.ba>
2023-01-03 13:12:52 +0100

    Clean up of code in coloringHBoxIdOnMouseClicked function

--- src/main/java/ba/unsa/etf/rpr/controllers/MainWindowController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainWindowController.java
@@ -35,15 +35,7 @@ public class MainWindowController {
         o.openWindow(borderPaneId,"styling");
     }
     public void coloringHBoxIdOnMouseClicked(MouseEvent mouseEvent) throws IOException {
-        final Stage registrationStage=(Stage) borderPaneId.getScene().getWindow();
-        Stage myStage=new Stage();
-        FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/coloring.fxml"));
-        loader.load();
-        myStage.setTitle("LogIn");
-        myStage.getIcons().add(new Image("/img/loginlogo.png"));
-        myStage.setScene(new Scene(loader.getRoot(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        myStage.show();
-        registrationStage.hide();
+        o.openWindow(borderPaneId,"coloring");
     }
     public void treatmentHBoxIdOnMouseClicked(MouseEvent mouseEvent) throws IOException {
         final Stage registrationStage=(Stage) borderPaneId.getScene().getWindow();
