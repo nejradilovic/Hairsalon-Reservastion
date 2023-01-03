dc09481b08f2c8fc3ea23385af8a794cf5a00a52
Nejra <nadilovic2@etf.unsa.ba>
2023-01-03 17:53:02 +0100

    Added attributes and methods in AboutUsController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/AboutUsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AboutUsController.java
@@ -1,5 +1,27 @@
 package ba.unsa.etf.rpr.controllers;
 
-public class AboutUsController {
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 
+public class AboutUsController {
+    public BorderPane borderPaneId;
+    public ImageView logoutIcon;
+    public Button logoutId;
+    public Button appointmentId;
+    public ImageView appointmentImageId;
+
+    public void logoutOnMouseClicked(MouseEvent mouseEvent) {
+    }
+
+    public void logoutOnAction(ActionEvent actionEvent) {
+    }
+
+    public void appointmentOnMouseClicked(MouseEvent mouseEvent) {
+    }
+
+    public void appointmentImageOnMouseClicked(MouseEvent mouseEvent) {
+    }
 }
