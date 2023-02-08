8b0bec32995b944e6dd71ba67caed9be4154457e
Nejra <nadilovic2@etf.unsa.ba>
2023-02-08 23:02:30 +0100

    Added checkUsername function in RegistrationController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -1,10 +1,18 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.dao.UserDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.HairsalonException;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static ba.unsa.etf.rpr.dao.AbstractDao.getConnection;
 
 
 public class RegistrationController{
@@ -20,7 +28,19 @@ public class RegistrationController{
     public GridPane gridPaneRegistration;
     OpenNewStage o=new OpenNewStage();
     public void initialize(){
-        Platform.runLater(()->firstnameEntry.setFocusTraversable(false));
+        usernameEntry.setFocusTraversable(false);
+        passwordEntry.setFocusTraversable(false);
+        firstnameEntry.setFocusTraversable(false);
+        lastnameEntry.setFocusTraversable(false);
+        emailEntry.setFocusTraversable(false);
+        phoneEntry.setFocusTraversable(false);
+        firstnameEntry.textProperty().addListener((obs, oldText, newText) -> {
+            lastnameEntry.setFocusTraversable(true);
+            emailEntry.setFocusTraversable(true);
+            phoneEntry.setFocusTraversable(true);
+            usernameEntry.setFocusTraversable(true);
+            passwordEntry.setFocusTraversable(true);
+        });
         String username=usernameEntry.getText();
         usernameEntry.textProperty().addListener((obs, oldValue, newValue)->{
             if(newValue.length()>=6)
@@ -47,5 +67,19 @@ public class RegistrationController{
                 && phoneEntry.getText().isBlank()==false && passwordEntry.getText().isBlank()==false){
             o.openWindow(gridPaneRegistration, "welcome");
         }
+    }
+    public boolean checkUsername(String username) {
+        String sql = "SELECT * FROM USER WHERE username = ?";
+        try {
+            PreparedStatement s=getConnection().prepareStatement(sql);
+            s.setString(1, username);
+            ResultSet r = s.executeQuery();
+            while(r.next()){
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
