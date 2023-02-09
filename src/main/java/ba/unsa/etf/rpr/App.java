12f6a6b89f1fd4cfead6f5d2929f12fbe4069185
Nejra <nadilovic2@etf.unsa.ba>
2023-02-09 19:58:25 +0100

    Added function printFormattedOptions in App class

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -14,11 +14,17 @@ import java.util.*;
 
 public class App {
     private static final Option getUsers = new Option("getU", "get-users", false, "Printing all users from database");
-
     private static final Option getAppointments = new Option("getA", "get-appointments", false, "Printing all appointments from database");
     private static final Option getStylists = new Option("getS", "get-stylists", false, "Printing all stylists from database");
     private static final Option addStylist = new Option("s", "add-stylist", false, "Adding a new stylist to database");
     private static final Option deleteStylist = new Option("delS", "delete-stylist", false, "Deleting a stylist from database");
     private static final Option updateStylist = new Option("update", "update-stylist",false, "Updating stylist from database");
+    public static void printFormattedOptions(Options options) {
+        HelpFormatter helpFormatter = new HelpFormatter();
+        PrintWriter printWriter = new PrintWriter(System.out);
+        helpFormatter.printUsage(printWriter, 150, "java -jar projekatRPR-cli-jar-with-dependencies.jar [option] (parameters)");
+        helpFormatter.printOptions(printWriter, 150, options, 2, 7);
+        printWriter.close();
+    }
 
 }
