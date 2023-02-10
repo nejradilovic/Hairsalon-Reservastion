4a3540ab6cf377ffac9b66bc5c894c078ee402d7
Nejra <nadilovic2@etf.unsa.ba>
2023-02-10 02:09:26 +0100

    Added javadoc comments in AboutUsController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/AboutUsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AboutUsController.java
@@ -7,7 +7,10 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
-
+/**
+ * AboutUsController gives you information about the app
+ * @author Nejra Adilovic
+ */
 public class AboutUsController {
     public BorderPane borderPaneId;
     public ImageView logoutIcon;
@@ -15,19 +18,31 @@ public class AboutUsController {
     public Button appointmentId;
     public ImageView appointmentImageId;
     OpenNewStage o= new OpenNewStage();
-
+    /**
+     * Opens the login window when you press on the logout icon
+     * @throws IOException when there is a problem with loading the FXML file.
+     */
     public void logoutOnMouseClicked(MouseEvent mouseEvent) throws IOException {
         o.openWindow(borderPaneId,"login");
     }
-
+    /**
+     * Opens the login window when you press on logout button
+     * @throws IOException when there is a problem with loading the FXML file.
+     */
     public void logoutOnAction(ActionEvent actionEvent) throws IOException {
         o.openWindow(borderPaneId,"login");
     }
-
+    /**
+     * Opens the my appointments window when you press on the text that says "My appointments"
+     * @throws IOException when there is a problem with loading the FXML file.
+     */
     public void appointmentOnMouseClicked(MouseEvent mouseEvent) throws IOException {
         o.openWindow(borderPaneId,"mainWindow");
     }
-
+    /**
+     * Opens the my appointments window when you press on the icon calendar
+     * @throws IOException when there is a problem with loading the FXML file.
+     */
     public void appointmentImageOnMouseClicked(MouseEvent mouseEvent) throws IOException {
         o.openWindow(borderPaneId,"mainWindow");
     }
