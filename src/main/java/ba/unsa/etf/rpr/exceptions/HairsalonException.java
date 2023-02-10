d49eae51e6b95429a15fa066a0508a2708303239
Nejra <nadilovic2@etf.unsa.ba>
2023-02-10 01:27:14 +0100

    Added javadoc comments in HairsalonException.java class

--- src/main/java/ba/unsa/etf/rpr/exceptions/HairsalonException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/HairsalonException.java
@@ -1,5 +1,9 @@
 package ba.unsa.etf.rpr.exceptions;
-
+/**
+ * HairsalonException is a custom exception class for handling errors related to the projects operations.
+ * It extends the base Exception class and provides two constructors for creating instances of the exception.
+ * @author Nejra Adilovic
+ */
 public class HairsalonException extends Exception{
 
     public HairsalonException(String msg, Exception reason){
