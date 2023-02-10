8900839e4da55a2b7a63bade8adacb02a90509f8
Nejra <nadilovic2@etf.unsa.ba>
2023-02-10 15:09:30 +0100

    Added getByFirstName method in UserDao.java class

--- src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -1,6 +1,9 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.HairsalonException;
+
+import java.util.List;
 
 /**
  * Dao interface for User domain bean
@@ -8,4 +11,6 @@ import ba.unsa.etf.rpr.domain.User;
  * @author Nejra Adilović
  */
 public interface UserDao extends Dao<User> {
+    User getByFirstName(String first_name) throws HairsalonException;
+
 }
