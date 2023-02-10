ba6cdf6208232fd44784191b27973c3a06e95416
Nejra <nadilovic2@etf.unsa.ba>
2023-02-10 02:12:37 +0100

    Added javadoc comments in TreatmentController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/TreatmentController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/TreatmentController.java
@@ -1,4 +1,7 @@
 package ba.unsa.etf.rpr.controllers;
-
+/**
+ *TreatmentController class is responsible for scheduling an appointment for hair treatment.
+ *@author Nejra Adilovic
+ */
 public class TreatmentController {
 }
