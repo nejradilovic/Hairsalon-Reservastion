1ec7cbf76a45b82eaddfea3a5d57a0417c5afe82
Nejra <nadilovic2@etf.unsa.ba>
2023-02-10 02:12:16 +0100

    Added javadoc comments in StylingController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/StylingController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/StylingController.java
@@ -1,4 +1,7 @@
 package ba.unsa.etf.rpr.controllers;
-
+/**
+ *StylingController class is responsible for scheduling an appointment for hair styling.
+ *@author Nejra Adilovic
+ */
 public class StylingController {
 }
