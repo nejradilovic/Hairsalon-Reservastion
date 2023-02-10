0d51bf7960e6c635b046504973882755331d90aa
Nejra <nadilovic2@etf.unsa.ba>
2023-02-10 01:53:11 +0100

    Added javadoc comments in WelcomeController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/WelcomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/WelcomeController.java
@@ -9,11 +9,17 @@ public class WelcomeController {
     public AnchorPane anchorPaneId;
     public ImageView aboutId;
     OpenNewStage o=new OpenNewStage();
-
+    /**
+     * Opens a dialog window with the provided FXML file path when you press on a certain part of the screen
+     *@throws IOException when there is a problem with loading the FXML file.
+     */
     public void mainWindowOnMouseClicked(MouseEvent mouseEvent) throws IOException {
         o.openWindow(anchorPaneId, "mainWindow" );
     }
-
+    /**
+     * Opens a dialog window with the provided FXML file path when you press on a certain part of the screen
+     *@throws IOException when there is a problem with loading the FXML file.
+     */
     public void aboutOnMouseClicked(MouseEvent mouseEvent) throws IOException {
         o.openWindow(anchorPaneId,"aboutUs");
     }
