08921876f0167c5cdca3d1d2ac0f6ceaa7cdd391
Nejra <nadilovic2@etf.unsa.ba>
2023-02-10 02:12:55 +0100

    Added javadoc comments in ColoringController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/ColoringController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ColoringController.java
@@ -1,4 +1,7 @@
 package ba.unsa.etf.rpr.controllers;
-
+/**
+ *ColoringController class is responsible for scheduling an appointment for hair coloring.
+ *@author Nejra Adilovic
+ */
 public class ColoringController {
 }
