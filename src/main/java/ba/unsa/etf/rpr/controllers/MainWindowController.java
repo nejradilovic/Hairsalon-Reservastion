1a1bdd2815a8c74f0d861659dfb9ee85a44ea14e
Nejra <nadilovic2@etf.unsa.ba>
2023-02-10 01:58:49 +0100

    Added javadoc comments in MainWindowController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/MainWindowController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainWindowController.java
@@ -9,6 +9,10 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 
 import java.io.IOException;
+/**
+ * MainWindowController class methods open a dialog window with the provided FXML file path depending on which hairsalon service you chose
+ * @throws IOException when there is a problem with loading the FXML file.
+ */
 public class MainWindowController {
     public Button logoutId;
     public BorderPane borderPaneId;
@@ -19,9 +23,17 @@ public class MainWindowController {
     public Button aboutTextId;
     public ImageView aboutImageId;
     OpenNewStage o=new OpenNewStage();
+    /**
+     * Opens the login window when you press on logout button
+     * @throws IOException when there is a problem with loading the FXML file.
+     */
     public void logoutOnAction(ActionEvent actionEvent) throws IOException {
         o.openWindow(borderPaneId,"login");
     }
+    /**
+     * Opens the login window when you press on the logout icon
+     * @throws IOException when there is a problem with loading the FXML file.
+     */
     public void logoutOnMouseClicked(MouseEvent mouseEvent) throws IOException {
         o.openWindow(borderPaneId,"login");
     }
@@ -37,11 +49,17 @@ public class MainWindowController {
     public void extensionsHBoxIdOnMouseClicked(MouseEvent mouseEvent) throws IOException {
         o.openWindow(borderPaneId,"extensions");
     }
-
+    /**
+     * Opens the about us window when you press on the text that says "About us"
+     * @throws IOException when there is a problem with loading the FXML file.
+     */
     public void aboutTextOnMouseClicked(MouseEvent mouseEvent) throws  IOException{
         o.openWindow(borderPaneId,"aboutUs");
     }
-
+    /**
+     * Opens the about us window when you press on the icon "About us"
+     * @throws IOException when there is a problem with loading the FXML file.
+     */
     public void aboutImageOnMouseClicked(MouseEvent mouseEvent) throws IOException {
         o.openWindow(borderPaneId,"aboutUs");
     }
