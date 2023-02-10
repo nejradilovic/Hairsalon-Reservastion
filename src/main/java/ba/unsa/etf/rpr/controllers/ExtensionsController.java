cdd3f8161c4c66f5e1ba8bd90e5f3e6210d562e6
Nejra <nadilovic2@etf.unsa.ba>
2023-02-10 02:12:02 +0100

    Added javadoc comments in ExtensionsController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/ExtensionsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ExtensionsController.java
@@ -1,4 +1,7 @@
 package ba.unsa.etf.rpr.controllers;
-
+/**
+ *ExtensionsController class is responsible for scheduling an appointment for hair extensions.
+ *@author Nejra Adilovic
+ */
 public class ExtensionsController {
 }
