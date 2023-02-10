b1f4380ca6bfddd8e6683aa446ebc83873d443ab
Nejra <nadilovic2@etf.unsa.ba>
2023-02-10 15:09:48 +0100

    Added getByLastName method in UserDao.java class

--- src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -12,5 +12,6 @@ import java.util.List;
  */
 public interface UserDao extends Dao<User> {
     User getByFirstName(String first_name) throws HairsalonException;
+    User getByLastName(String last_name) throws HairsalonException;
 
 }
