9817fafe0aef49c9fe64f76aa9c9b664e917fe96
Nejra <nadilovic2@etf.unsa.ba>
2023-02-11 19:26:11 +0100

    Added methods in UserDao.java class

--- src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -15,4 +15,18 @@ public interface UserDao extends Dao<User> {
     User getByLastName(String last_name) throws HairsalonException;
     User getByUsername(String username) throws HairsalonException;
     User getByAdmin(boolean admin) throws HairsalonException;
+    /**
+     * Returns user whose username and password are given as parameters.
+     * @param username search String for username of user
+     * @param password search String for password of user
+     * @return User instance
+     */
+    public User checkUser(String username, String password);
+
+    /**
+     * Returns true if username, given as parameter, exists in database.
+     * @param username search String for username
+     * @return boolean
+     */
+    public boolean checkUsername(String username);
 }
