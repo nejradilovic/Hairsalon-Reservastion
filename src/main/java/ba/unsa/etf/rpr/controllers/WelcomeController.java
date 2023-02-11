741d4983c94d3b2c051018f7b47a7e0ad8a1e9a2
Nejra <nadilovic2@etf.unsa.ba>
2023-02-11 19:20:23 +0100

    Code cleanup in WelcomeController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/WelcomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/WelcomeController.java
@@ -1,26 +1,49 @@
 package ba.unsa.etf.rpr.controllers;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class WelcomeController {
     public ImageView appointmentId;
     public AnchorPane anchorPaneId;
     public ImageView aboutId;
-    OpenNewStage o=new OpenNewStage();
     /**
      * Opens a dialog window with the provided FXML file path when you press on a certain part of the screen
      *@throws IOException when there is a problem with loading the FXML file.
      */
     public void mainWindowOnMouseClicked(MouseEvent mouseEvent) throws IOException {
-        o.openWindow(anchorPaneId, "mainWindow" );
+        openDialog("Home page", "/fxml/mainWindow.fxml", new MainWindowController());
     }
     /**
      * Opens a dialog window with the provided FXML file path when you press on a certain part of the screen
      *@throws IOException when there is a problem with loading the FXML file.
      */
     public void aboutOnMouseClicked(MouseEvent mouseEvent) throws IOException {
-        o.openWindow(anchorPaneId,"aboutUs");
+        openDialog("About us", "/fxml/aboutUs.fxml", new AboutUsController());
+    }
+    /**
+     * Opens a dialog window with the provided FXML file path
+     * @param title String for window Title
+     * @param file path of the FXML file
+     * @param controller Object
+     * @throws IOException in case of an error
+     */
+    private void openDialog(String title, String file, Object controller) throws IOException {
+        final Stage homeStage = (Stage) anchorPaneId.getScene().getWindow();
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(file));
+        loader.setController(controller);
+        stage.setTitle(title);
+        stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        homeStage.hide();
+        stage.show();
     }
 }
