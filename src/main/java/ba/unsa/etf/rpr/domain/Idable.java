e1ecad41b7c9efbb1b0a624c0b1b41e65aa5df61
Nejra <nadilovic2@etf.unsa.ba>
2023-02-13 10:38:44 +0100

    Added javadoc comments in Idable.java class

--- src/main/java/ba/unsa/etf/rpr/domain/Idable.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Idable.java
@@ -5,8 +5,14 @@ package ba.unsa.etf.rpr.domain;
  * @author Nejra Adilovic
  */
 public interface Idable {
-
+    /**
+     * sets or updates the value of id
+     * @param id int value
+     */
     void setId(int id);
-
+    /**
+     * gets the value of id
+     * @return int value
+     */
     int getId();
 }
