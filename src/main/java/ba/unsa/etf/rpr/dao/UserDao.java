93bf72325e2eb2dd03ea170b53b624d1b2b632cb
Nejra <nadilovic2@etf.unsa.ba>
2023-02-13 10:43:06 +0100

    Added javadoc comments in UserDao.java class

--- src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -3,17 +3,40 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.HairsalonException;
 
-import java.util.List;
 
 /**
  * Dao interface for User domain bean
  *
- * @author Nejra Adilović
+ * @author Nejra Adilovic
  */
 public interface UserDao extends Dao<User> {
+    /**
+     * Fetches User object from table USER defined by given first name.
+     * @param first_name String
+     * @return User
+     * @throws HairsalonException thrown in case of problem with db
+     */
     User getByFirstName(String first_name) throws HairsalonException;
+    /**
+     * Fetches User object from table USER defined by given last name.
+     * @param last_name String
+     * @return User
+     * @throws HairsalonException thrown in case of problem with db
+     */
     User getByLastName(String last_name) throws HairsalonException;
+    /**
+     * Fetches User object from table USER defined by given username.
+     * @param username String
+     * @return User
+     * @throws HairsalonException thrown in case of problem with db
+     */
     User getByUsername(String username) throws HairsalonException;
+    /**
+     * Fetches User object from table USER defined by given boolean depending on if the user is an admin.
+     * @param admin boolean
+     * @return User
+     * @throws HairsalonException thrown in case of problem with db
+     */
     User getByAdmin(boolean admin) throws HairsalonException;
     /**
      * Returns user whose username and password are given as parameters.
@@ -21,12 +44,20 @@ public interface UserDao extends Dao<User> {
      * @param password search String for password of user
      * @return User instance
      */
-    public User checkUser(String username, String password);
+    User checkUser(String username, String password);
 
     /**
      * Returns true if username, given as parameter, exists in database.
      * @param username search String for username
      * @return boolean
      */
-    public boolean checkUsername(String username);
+    boolean checkUsername(String username);
+    /**
+     * Fetches User object from table USER defined by username and password given as parameter
+     * @param username String value that represents username
+     * @param password String value that represents password
+     * @return int value for id
+     * @throws HairsalonException in case of an error
+     */
+    int getLoggedInId(String username, String password) throws HairsalonException;
 }
