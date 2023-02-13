5cc45a6f79cdee38897a74e7e44dabb85e5291ad
Nejra <nadilovic2@etf.unsa.ba>
2023-02-13 10:45:17 +0100

    Added javadoc comments in StylistTest.java class

--- src/test/java/ba/unsa/etf/rpr/domain/StylistTest.java
+++ src/test/java/ba/unsa/etf/rpr/domain/StylistTest.java
@@ -1,7 +1,7 @@
-package ba.unsa.etf.rpr.dao;
+package ba.unsa.etf.rpr.domain;
 
 import ba.unsa.etf.rpr.business.StylistManager;
-import ba.unsa.etf.rpr.domain.Stylist;
+import ba.unsa.etf.rpr.dao.StylistDao;
 import ba.unsa.etf.rpr.exceptions.HairsalonException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,24 +11,46 @@ import org.mockito.MockitoAnnotations;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 
-public class StylistDaoTest {
+/**
+ * Tests for Stylist class
+ * @author Nejra Adilovic
+ */
+public class StylistTest {
     private StylistManager stylistManager;
     /**
      * Mock test for Stylist
+     * StylistDao is annotated with @Mock, which means it will be mocked using the Mockito framework.
      */
     @Mock
     private StylistDao stylistDao;
     public Stylist stylist = new Stylist("Sara", "Biogradlic",  "062001333");
+
+    /**
+     * MockitoAnnotations.openMocks method is called to initialize the mock objects and a new instance of the StylistManager is created.
+     */
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
         stylistManager = new StylistManager();
     }
+
+    /**
+     * Tests the add method of the StylistDao
+     * Calls the add method with the stylist object as a parameter
+     * Verifies that the add method was called with the stylist object by calling verify(stylistDao).add(stylist)
+     * @throws HairsalonException in case of problems with db
+     */
     @Test
     public void addTest() throws HairsalonException {
         stylistDao.add(stylist);
         verify(stylistDao).add(stylist);
     }
+
+    /**
+     * Updates first_name of stylist object, calls the update method of StylistDao with the stylist object as a parameter
+     * Verifies that the update method was called with the stylist object by calling verify(stylistDao).update(stylist).
+     * @throws Exception in case of problems with db
+     */
     @Test
     void updateTest() throws Exception {
         stylist.setFirst_name("Sarah");
@@ -36,7 +58,7 @@ public class StylistDaoTest {
         verify(stylistDao).update(stylist);
     }
     /**
-     * Test for setters and getters for Stylist
+     * Test for setters and getters in Stylist
      */
     @Test
     public void testStylistSettersAndGetters() {
@@ -49,7 +71,7 @@ public class StylistDaoTest {
         assertEquals("062001333", stylist.getPhone());
     }
     /**
-     * Test for constructor for Stylist
+     * Test for constructor in Stylist
      */
     @Test
     public void testStylistConstructor() {
@@ -59,7 +81,7 @@ public class StylistDaoTest {
         assertEquals("062001333", stylist.getPhone());
     }
     /**
-     * Test for equals method for Stylist
+     * Test for equals method in Stylist
      */
     @Test
     public void testEquals() {
@@ -68,7 +90,7 @@ public class StylistDaoTest {
         assertEquals(stylist1, stylist2);
     }
     /**
-     * Test for toString method for Stylist
+     * Test for toString method in Stylist
      */
     @Test
     public void testToString()
