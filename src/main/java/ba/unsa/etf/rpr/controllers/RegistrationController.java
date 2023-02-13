52498448f26dd55a65059cc8c80298a84ef12576
Nejra <nadilovic2@etf.unsa.ba>
2023-02-13 10:32:58 +0100

    Some changes in RegistrationController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -13,6 +13,9 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 /**
@@ -25,6 +28,7 @@ public class RegistrationController{
     public TextField emailEntry;
     public TextField phoneEntry;
     public TextField usernameEntry;
+    public Label emailMessage;
     public PasswordField passwordEntry;
     public Button signupButton;
     public Hyperlink loginLink;
@@ -32,6 +36,7 @@ public class RegistrationController{
     public GridPane gridPaneRegistration;
     /**
      * Listener that makes sure that the username is over 6 characters
+     * Removes focus from fields
      */
     public void initialize(){
         usernameEntry.setFocusTraversable(false);
@@ -53,6 +58,21 @@ public class RegistrationController{
             else if(newValue.length()<6)
                 invalidUsername.setText("Username must have atleast 6 characters");
         });
+    }
+    /**
+     * checkEmail method checks if a given string is a valid email address
+     * The method uses a regular expression to validate the email address.
+     * @param emailField The string to be checked if it is a valid email address.
+     * @return returns true if the given string is a valid email address, false otherwise
+     */
+    public boolean checkEmail(String emailField){
+        String regex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(emailField);
+        return matcher.matches();
     }
     /**
      * Opens the login window when you press on the login link
@@ -92,14 +112,33 @@ public class RegistrationController{
                 invalidUsername.setText("Username already exists!");
             }
             else {
-                user.setPassword(passwordEntry.getText());
-                user.setUsername(usernameEntry.getText());
-                user.setLast_name(lastnameEntry.getText());
-                user.setFirst_name(firstnameEntry.getText());
-                user.setPhone(phoneEntry.getText());
-                user.setEmail(emailEntry.getText());
-                u.add(user);
-                openDialog("Welcome", "/fxml/welcome.fxml", new WelcomeController());
+                boolean emailFlag=checkEmail(emailEntry.getText());
+                if(!emailFlag) {
+                    emailMessage.setText("Invalid e-mail format.");
+                    emailEntry.clear();
+                }
+                else {
+                    user.setPassword(passwordEntry.getText());
+                    user.setUsername(usernameEntry.getText());
+                    user.setLast_name(lastnameEntry.getText());
+                    user.setFirst_name(firstnameEntry.getText());
+                    user.setPhone(phoneEntry.getText());
+                    user.setEmail(emailEntry.getText());
+                    u.add(user);
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Successful registration");
+                    alert.setHeaderText(user.getUsername() + " you have successfully signed up!");
+                    alert.showAndWait();
+                    final Stage mainStage = (Stage) gridPaneRegistration.getScene().getWindow();
+                    Stage myStage = new Stage();
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+                    loader.load();
+                    myStage.setTitle("Seat&Style");
+                    myStage.getIcons().add(new Image("/img/loginlogo.png"));
+                    myStage.setScene(new Scene(loader.getRoot(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+                    myStage.show();
+                    mainStage.hide();
+                }
             }
         }
     }
@@ -115,6 +154,7 @@ public class RegistrationController{
         Stage stage = new Stage();
         FXMLLoader loader = new FXMLLoader(getClass().getResource(file));
         loader.setController(controller);
+        stage.getIcons().add(new Image("/img/loginlogo.png"));
         stage.setTitle(title);
         stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         stage.setResizable(false);
