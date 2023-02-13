38175116682f18229d8b0b9f52dbfe4f060f6a9a
Nejra <nadilovic2@etf.unsa.ba>
2023-02-13 10:42:15 +0100

    Added javadoc comments in StylistDao.java class

--- src/main/java/ba/unsa/etf/rpr/dao/StylistDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/StylistDao.java
@@ -1,6 +1,9 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Stylist;
+import ba.unsa.etf.rpr.exceptions.HairsalonException;
+
+import java.util.List;
 
 /**
  * Dao interface for Stylist domain bean
@@ -8,4 +11,18 @@ import ba.unsa.etf.rpr.domain.Stylist;
  * @author Nejra Adilović
  */
 public interface StylistDao extends Dao<Stylist> {
+    /**
+     * Fetches Stylist object form table STYLIST defined by given name (first_name and last_name).
+     * @param first_name String
+     * @param last_name String
+     * @return Stylist object defined by given name
+     * @throws HairsalonException thrown in case of problem with db
+     */
+    Stylist getByName(String first_name, String last_name) throws HairsalonException;
+    /**
+     * Fetches all names of stylists from table STYLIST and stores it in a list.
+     * @return List of all stylists names
+     * @throws HairsalonException thrown in case of problem with db
+     */
+    List<String> getAllNames() throws HairsalonException;
 }
